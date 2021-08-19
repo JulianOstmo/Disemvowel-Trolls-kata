70b8f3835cfcc3ac94dda4f31e02b8f5b5f45780
Julian Ostmo <julian.ostmo@ibm.com>
2021-08-19 17:19:53 +0100

    feat: kata complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 const neutralizeThreat = (input) => {
+  if (typeof input !== 'string') {
+    throw Error('Input must be a string');
+  }
+
+  if (input === '') {
+    return input;
+  }
+
   return input.replace(/[A|E||I|O|U]/gi, '');
 };
 
