1db56d72a8c8f0f6bbf0ee4ab30ccd3dc2e4d48f
Julian Ostmo <julian.ostmo@ibm.com>
2021-08-19 11:21:57 +0100

    feat: pomodoro 1 - UAT3 - red

--- test/index.test.js
+++ test/index.test.js
@@ -16,12 +16,16 @@ describe('GIVEN the neutralizeThreat function', () => {
   });
 
   describe('WHEN the input is a valid string', () => {
-    test('THEN "LL" to be returned when "LOL" is passed', () => {
+    test('THEN "LL" should be returned when "LOL" is passed', () => {
       expect(neutralizeThreat('LOL')).toBe('LL');
     });
 
-    test('THEN "ll" to be returned when "lol" is passed', () => {
+    test('THEN "ll" should be returned when "lol" is passed', () => {
       expect(neutralizeThreat('lol')).toBe('ll');
+    });
+
+    test('THEN "lsrs LL" should be returned when "losers LOL" passed', () => {
+      expect(neutralizeThreat('losers LOL')).toBe('lsrs LL');
     });
   });
 });
